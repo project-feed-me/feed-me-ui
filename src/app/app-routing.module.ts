5cfe0ab6b92224002701273c97ff31fc42398795
DESKTOP-F9UTEN3\Crayton <crayton1995@yahoo.com>
2018-03-11 13:55:00 -0500

    Added routing to 2 different views using 'router-outlet'. Setup 2 starting components (login and cooking type) Added support for bootstap, currently v4. Might change that to v3. Added support for SCSS.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from '@angular/router';
+
+import { CookTypeComponent } from './cook-food-type/cook-type.component';
+import { loginComponent } from './login/login.component';
+
+const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch:'full'},
+    {path: 'login', component: loginComponent },
+    {path: 'cooking-type', component: CookTypeComponent}
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
