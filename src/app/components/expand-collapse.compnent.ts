8d581c8d1f57a8f8290f6020349759118f432a1f
DESKTOP-F9UTEN3\Crayton <crayton1995@yahoo.com>
2018-03-27 22:51:57 -0500

    pushing expand collapse cmp. set max width for page to be mobile so on desktop it does not look terrible. set up some styles for the ingredient details.

---
+++ src/app/components/expand-collapse.compnent.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'expand-collapse-component',
+  template: `
+<div class="sf-expand-collapse-wrapper container">
+  <div class="sf-expand-collapse-header-container clearfix">
+    <div class="sf-expand-collapse-name" [innerHTML]="recipe.name"></div>
+    <div class="sf-expand-collapse-arrow">
+      <span class="fas fa-angle-down" (click)="toggleExpandCollapse()"></span>
+    </div>
+  </div>
+  <div class="sf-expand-collapse-body-container" [class.expanded]="expanded">
+    <div class="sf-expand-collapse-body-ingredients clearfix">
+      <div class="name-placeholder">Ingredients:</div>
+      <ul>
+        <li *ngFor="let ingredient of recipe.ingredients" [innerHTML]="ingredient"></li>
+      </ul>
+    </div>  
+    <span class="sf-expand-collapse-body-description" [innerHTML]="recipe.description"></span>
+  </div>
+</div>
+  `,
+})
+export class ExpandCollapseComponent {
+  @Input() recipe: any;
+  @Input() index: string;
+  expanded: boolean = false;
+  toggleExpandCollapse() {
+    this.expanded = this.expanded === false ? true : false;
+  }
+}
--- src/app/recipe-dashboard/recipe-dashboard.component.ts
+++ src/app/recipe-dashboard/recipe-dashboard.component.ts
@@ -5,7 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './recipe-dashboard.component.html',
 })
 export class RecipeDashboardComponent implements OnInit {
-
+  mockRecipeData: any = [
+    {
+      name: "Apple Chicken",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      ingredients: ["2 tbs Taco seasoning", "4 chicken breast", "8 oz chicken stock", "1 tsp salt", "1 tsp pepper", "1 tsp chili powder"]
+    },
+    {
+      name: "APRICOT CHICKEN",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      ingredients: ["1 tbs Taco seasoning", "2 chicken breast", "4 oz chicken stock", "4 tsp salt", "7 tsp pepper", "10 tsp chili powder"]
+    },    {
+      name: "Azzzz Chicken",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      ingredients: ["6 tbs Taco seasoning", "20 chicken breast", "10 oz chicken stock", "12 tsp salt", "14 tsp pepper", "15 tsp chili powder"]
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
